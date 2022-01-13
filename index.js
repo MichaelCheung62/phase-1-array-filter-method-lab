55671ebdbaf078d3bf0849353df4b41ac2d90431
Michael <MichaelCheung101@gmail.com>
2022-01-12 20:07:39 -0500

    commit tree 6d23575832e6fdd9bf302d0823ff2b5ab014f7e3

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+/*
+ 1.define function 
+ 2.determine how many parameters
+ 3.find,map,filter& reduce - methods for manipulating arrays
+*/
+
+const findMatching = (drivers,name) => {
+     return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+
+}
+
+
+
+const fuzzyMatch = (drivers,name) => {
+    return drivers.filter(driver => driver[0] === name[0])
+
+}
+const matchName = (drivers,name) => {
+    return drivers.filter(driver => driver.name === name)
+}
